3e0b773f153a43daf048cb99982ad2a0ce6fbc0f
Devin Mounts and Nate Cottle <devin@>
2018-06-04 10:35:24 -0700

    add javascript with for loop to return values to ul.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,40 @@
-$(function(){
-  $("#submit").click(function(event){
-      var email = $("#inputEmail").val();
-      var name = $("#inputName").val();
-      var color = $("#inputColor").val();
-      var animal = $("#inputAnimal").val();
-      var array1 = [email, name, color, animal];
-      alert(array1);
+function makeArray() {
+  var email = $("#inputEmail").val();
+  var name = $("#inputName").val();
+  var color = $("#inputColor").val();
+  var animal = $("#inputAnimal").val();
+  var array1 = [email, name, color, animal];
+  var access1 = array1[0];
+  var access2 = array1[1];
+  var access3 = array1[2];
 
-      event.preventDefault();
+  array1.push(access1, access2, access3);
+
+  var arLength = array1.length;
+
+  var newArray = array1.slice(0, arLength);
+  //console.log(array1);
+  console.log(newArray);
+
+
+  //$("#return").prepend($("<li>")newArray);
+  // newArray.forEach(function(item){
+  //   $("#return").prepend("<li>"+ item + "</li>");
+  // });
+
+  for (x=0; x<newArray.length; x++) {
+    $("#return").prepend("<li>"+ newArray[x] + "</li>");
+  };
+  //$("#return").prepend("<li>"+ newArray + "</li>");
+}
+
+
+
+
+$(function() {
+  $("#submit").click(function(event) {
+    event.preventDefault();
+    makeArray();
   });
 
 });
